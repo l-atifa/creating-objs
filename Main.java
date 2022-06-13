c2c2996ea0b6f69c4561e9624bf336645299abea
Latifa M <94848902+l-atifa@users.noreply.github.com>
2022-06-13 13:06:39 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import static java.lang.System.*;
+
+import java.util.Scanner;
+
+
+class Main {
+  public static void main(String[] args) {
+           
+    String x = "HELLO";
+    String y = "hello";
+      
+    if (x.equals(y))
+    {
+      System.out.println("Equal strings");
+    }
+  }
+}
